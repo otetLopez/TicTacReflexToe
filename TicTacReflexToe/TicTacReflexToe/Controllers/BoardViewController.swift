f47919afb6c398ceb10c0bd058c0a412eb47a424
otetLopez <otet.tud@gmail.com>
2020-08-12 12:07:26 -0400

    Add Button actions

---
+++ TicTacReflexToe/TicTacReflexToe/Controllers/BoardViewController.swift
@@ -0,0 +1,26 @@
+//
+//  BoardViewController.swift
+//  TicTacReflexToe
+//
+//  Created by otet_tud on 8/12/20.
+//  Copyright © 2020 otet. All rights reserved.
+//
+
+import UIKit
+
+class BoardViewController: UIViewController {
+    var activePlayer = 1
+    var activeGame = true
+    var gameState = [0,0,0,0,0,0,0,0,0]
+    let winnigCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    @IBAction func btn_move(_ sender: UIButton) {
+        print("Button is pressed")
+    }
+}

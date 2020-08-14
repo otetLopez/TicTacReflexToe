a5ae31c7266abd363ef9076df870bde02c9ddf2d
otetLopez <otet.tud@gmail.com>
2020-08-14 12:02:09 -0400

    Add checking in register field

--- TicTacReflexToe/TicTacReflexToe/Controllers/RegisterViewController.swift
+++ TicTacReflexToe/TicTacReflexToe/Controllers/RegisterViewController.swift
@@ -13,6 +13,8 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var tf_email: UITextField!
     @IBOutlet weak var tf_pwd: UITextField!
     @IBOutlet weak var tf_cpwd: UITextField!
+    @IBOutlet weak var saveBtn: UIButton!
+    @IBOutlet weak var cancelBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +32,10 @@ class RegisterViewController: UIViewController {
         self.navigationController?.setNavigationBarHidden(false, animated: true)
         self.navigationController?.setToolbarHidden(true, animated: true)
         self.navigationItem.hidesBackButton = true
+        
+        saveBtn.layer.cornerRadius = 10
+        cancelBtn.layer.cornerRadius = 10
+        
         clearFields()
     }
 
@@ -41,6 +47,10 @@ class RegisterViewController: UIViewController {
     }
 
     @IBAction func save_btn_pressed(_ sender: UIButton) {
+        if(checkFields()) {
+            // Create new user
+            
+        }
     }
 
     @IBAction func cancel_btn_pressed(_ sender: UIButton) {
@@ -68,4 +78,78 @@ class RegisterViewController: UIViewController {
         tf_pwd.text = ""
         tf_cpwd.text = ""
     }
+    
+    func checkFields() -> Bool {
+        var isIncomplete : Bool = false
+        if tf_uname.text!.isEmpty {
+            tf_uname.layer.borderWidth = 1
+            tf_uname.layer.borderColor = UIColor.red.cgColor
+            isIncomplete = true
+        } else {
+            tf_uname.layer.borderWidth = 0
+        }
+        if tf_email.text!.isEmpty {
+            tf_email.layer.borderWidth = 1
+            tf_email.layer.borderColor = UIColor.red.cgColor
+            isIncomplete = true
+        } else {
+            tf_email.layer.borderWidth = 0
+        }
+        if tf_pwd.text!.isEmpty {
+            tf_pwd.layer.borderWidth = 1
+            tf_pwd.layer.borderColor = UIColor.red.cgColor
+            isIncomplete = true
+        } else {
+            tf_pwd.layer.borderWidth = 0
+        }
+        if tf_cpwd.text!.isEmpty {
+            tf_cpwd.layer.borderWidth = 1
+            tf_cpwd.layer.borderColor = UIColor.red.cgColor
+            isIncomplete = true
+        } else {
+            tf_cpwd.layer.borderWidth = 0
+        }
+
+        if(isIncomplete == true) {
+            alertErrors(msg: "Missing fields")
+        } else {
+            /* Check passwords */
+            if isPasswordValid() == true {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func isPasswordValid() -> Bool {
+        var isPwdValid : Bool = true
+        /* Check if password is Strong */
+        if tf_pwd.text?.count ?? 0 < 6 {
+            print("DEBUG: RegisterViewController isPasswordValid() Password Weak")
+            alertErrors(msg: "Password Weak")
+            isPwdValid = false
+        } else if tf_pwd.text! != tf_cpwd.text! {
+            print("DEBUG: RegisterViewController isPasswordValid() Passwords did not match")
+            alertErrors(msg: "Passwords did not match")
+            isPwdValid = false
+        }
+        
+        if isPwdValid == false {
+            tf_pwd.layer.borderWidth = 1
+            tf_pwd.layer.borderColor = UIColor.red.cgColor
+            tf_cpwd.layer.borderWidth = 1
+            tf_cpwd.layer.borderColor = UIColor.red.cgColor
+        } else {
+            tf_pwd.layer.borderWidth = 0
+            tf_cpwd.layer.borderWidth = 0
+        }
+        return isPwdValid
+    }
+    
+    func alertErrors(msg: String) {
+        let alertController = UIAlertController(title: "Error", message: msg, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 }

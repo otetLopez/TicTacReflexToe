02beea1b11db5599ae0bfa42ca739f63ebc118a5
otetLopez <otet.tud@gmail.com>
2020-08-14 12:14:42 -0400

    Adding methods to user class

--- TicTacReflexToe/TicTacReflexToe/Classes/User.swift
+++ TicTacReflexToe/TicTacReflexToe/Classes/User.swift
@@ -23,12 +23,50 @@ class User : CustomStringConvertible {
         self.score = 0
     }
     
+    init(uname : String, eadd : String, pwd : String) {
+        self.username = uname
+        self.email = eadd
+        self.passwd = pwd
+        self.score = 0
+    }
+    
     init(uname : String, eadd : String, pwd : String, score : Int) {
         self.username = uname
         self.email = eadd
         self.passwd = pwd
         self.score = score
     }
+    
+    
+    public func addScore() {
+        self.score = self.score +  1
+    }
+    
+    public func addScore(points : Int) {
+        self.score = self.score + points
+    }
+    
+    
+    /* Getters */
+    public func getUname() -> String { return self.username }
+    
+    public func getEmail() -> String { return self.email }
+    
+    public func getPassword() -> String { return self.passwd }
+    
+    public func getScore() -> Int { return self.score }
+    
+    
+    /* Setters */
+    public func setUname(uname: String) { self.username = uname }
+    
+    public func setEmail(eadd: String) { self.email = eadd }
+    
+    public func setPassword(pwd: String) { self.passwd = pwd }
+
+    public func setScore(score: Int) { self.score = score }
+    
+    
     
     var description: String {
         var detailStr = "--------------------------------\nName: \(self.username)\nEmail: \(self.email)\nPassword: \(self.passwd)\nScore: \(self.score)\n"
